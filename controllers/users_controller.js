e0048734540542046f8fe10eaf76ccaa7f4a2b33
AjayBiradar5956 <ajay99biradar@gmail.com>
2023-09-06 00:08:40 +0530

    modify customer completed

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,19 +1,19 @@
 const axios = require('axios');
 const cookie = require('cookie');
+const api = 'https://qa2.sunbasedata.com/sunbase/portal/api/assignment.jsp?';
 
 module.exports.createSession = function (req, res) {
     const token = req.bearerToken;
-    console.log(token);
     const headers = {
         Authorization: `Bearer ${token}`,
     };
-    const apiGetUrl = 'https://qa2.sunbasedata.com/sunbase/portal/api/assignment.jsp?cmd=get_customer_list';
+    const params = 'cmd=get_customer_list'
+    const apiGetUrl = `${api}${params}`;
 
     axios.get(apiGetUrl, { headers })
         .then(async (response) => {
             if (response.status == 200) {
                 const customerList = await response.data;
-                console.log(customerList[0]);
                 res.render('customer', {
                     title: 'Customer Info',
                     customerList,
@@ -28,11 +28,8 @@ module.exports.createSession = function (req, res) {
 }
 
 module.exports.delete = function (req, res) {
-    console.log("delete hit");
-
     const cookies = cookie.parse(req.headers.cookie || '');
     const token = cookies.access_token || '';
-
     const headers = {
         Authorization: `Bearer ${token}`,
     }
@@ -52,7 +49,7 @@ module.exports.delete = function (req, res) {
             } else if (response.status === 400) {
                 return res.status(400).json({ message: 'UUID not found' });
             } else {
-                return res.status(500).json({ message: 'Error deleting customer' });
+                return res.status(500).json({ message: 'Error Not Deleted' });
             }
         })
         .catch((err) => {
@@ -63,10 +60,21 @@ module.exports.delete = function (req, res) {
 module.exports.addPage = function (req, res) {
     return res.render('addCustomer', {
         title: "Add Customer",
+        addPage: true,
+    })
+}
+
+module.exports.updatePage = function (req, res) {
+    const id = req.params.id;
+    return res.render('addCustomer', {
+        title: "Add Customer",
+        addPage: false,
+        id,
     })
 }
 
 module.exports.addCustomer = function (req, res) {
+    const uuid = Date.now();
     const { firstname, lastname, street, address, city, state, email, phone } = req.body;
 
     const customerData = {
@@ -78,11 +86,13 @@ module.exports.addCustomer = function (req, res) {
         state,
         email,
         phone,
+        uuid,
     }
+
+    console.log("Add handler", customerData);
 
     const cookies = cookie.parse(req.headers.cookie || '');
     const token = cookies.access_token || '';
-    console.log("cookie", token);
 
     // Define the API URL for adding a customer
     const apiCreateUrl =
@@ -103,9 +113,9 @@ module.exports.addCustomer = function (req, res) {
             if (response.status === 201) {
                 return res.status(201).json({ message: 'Successfully Created' });
             } else if (response.status === 400) {
-                return res.status(400).json({ message: 'Bad Request: First Name or Last Name is missing' });
+                return res.status(400).json({ message: 'First Name or Last Name is missing' });
             } else {
-                console.error('Add Customer API Error:', response.data); // Log the error response
+                console.error('Add Customer API Error:', response.data);
                 return res.status(500).json({ message: 'Error creating customer' });
             }
         })
@@ -113,4 +123,54 @@ module.exports.addCustomer = function (req, res) {
             console.error('Add Customer API Error:', err.message); // Log the Axios error
             return res.status(500).json({ message: 'Internal server error' });
         });
+}
+
+
+module.exports.modifyCustomer = function (req, res) {
+    const { firstname, lastname, street, address, city, state, email, phone } = req.body;
+
+    const customerData = {
+        first_name: firstname,
+        last_name: lastname,
+        street,
+        address,
+        city,
+        state,
+        email,
+        phone,
+        uuid: req.params.id,
+    }
+    console.log("Modify handler", customerData);
+
+    const requestBody = {
+        ...customerData,
+    }
+
+    const id = req.params.id;
+
+    const cookies = cookie.parse(req.headers.cookie || '');
+    const token = cookies.access_token || '';
+
+    const headers = {
+        Authorization: `Bearer ${token}`,
+    }
+
+    const apiModifyUrl = `https://qa2.sunbasedata.com/sunbase/portal/api/assignment.jsp?cmd=update&uuid=${id}`;
+
+    axios.post(apiModifyUrl, requestBody, { headers })
+        .then((response) => {
+            if (response.status == 200) {
+                res.status(200).json({ message: 'Successfully Updated' });
+            } else if (response.status === 400) {
+                return res.status(400).json({ message: 'Body is Empty' });
+            } else {
+                console.error('Add Customer API Error:', response.data);
+                return res.status(500).json({ message: 'UUID not found' });
+            }
+        })
+        .catch((err) => {
+            console.error('Error while updating details', err.message);
+            res.status(500).json({ message: "Internal Server Error" });
+        })
+
 }
--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,9 @@ const authMiddleware = require('../config/authMiddleware');
 router.post('/create-session', authMiddleware.authenticateUser, users_controller.createSession);
 router.post('/delete/:id', authMiddleware.authenticateUser, users_controller.delete);
 router.post('/addCustomer', authMiddleware.authenticateUser, users_controller.addCustomer);
+router.post('/modifyCustomer/:id', authMiddleware.authenticateUser, users_controller.modifyCustomer);
 
 router.get('/addPage', users_controller.addPage);
+router.get('/updatePage/:id', users_controller.updatePage);
 
 module.exports = router;
